9dcf071ed781ce025242c004e32c0e58ff5ba482
zhuli <1226950042@qq.com>
2019-05-01 23:10:10 +0800

    the second commit

--- quickstart/src/Api/Controllers/IdentityController.cs
+++ quickstart/src/Api/Controllers/IdentityController.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+
 [Route("identity")]
 [Authorize]
 public class IdentityController : ControllerBase
 {
     [HttpGet]
-    public IActionResult GetAction()
+    public IActionResult Get()
     {
         return new JsonResult(from c in User.Claims select new {c.Type, c.Value});
     }
--- quickstart/src/Api/Startup.cs
+++ quickstart/src/Api/Startup.cs
@@ -15,20 +15,24 @@ namespace Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.Audience = "api1";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseAuthentication();
+            app.UseMvc();
         }
     }
 }
